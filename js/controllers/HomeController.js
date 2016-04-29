e7adb8d466f097c41b7f56f6002a894b6e83ae74
Brittany Hoban <brittanyhoban@yahoo.com>
2016-04-28 23:29:20 -0400

    Step 9: Upday imageinfo
    display the number of the current poster and the total number of
    posters for each movie. This should change every time a poster changes
    (if there is more then one poster for that movie). Example: Poster 2 of
    3

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -185,10 +185,9 @@ app.controller('HomeController', ['$scope', function($scope) {
   {
    
   
- $scope.hours = (Math.floor(Math.abs(minutes) / 60));  
-$scope.minutes = (Math.abs(minutes) % 60);  
-  
- return  $scope.hours +'hrs '+$scope.minutes + 'min';  
+      $scope.hours = (Math.floor(Math.abs(minutes) / 60));  
+      $scope.minutes = (Math.abs(minutes) % 60);  
+      return  $scope.hours +'hrs '+$scope.minutes + 'min';  
   
     } ; 
   
